04fda9ce744821f5319834295270febbda39a968
Karen Schulz <sirk.schulz@gmail.com>
2021-02-08 16:47:22 +0000

    created index.php and started with the htmal basic things as head, body, nav_bar, etc

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+?>
+
+<html lang="en-GB">
+
+	<head>
+		<title> Welcome!</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+
+<body>
+<nav class="nav_bar">
+    <p class="welcome">Welcome to my own personal Plant Database</p>
+    <div class="links_nav">
+        <a href="form.php">Add new plants to the database</a>
+    </div>
+</nav>
+
+<main>
+    <h1>Hi!</h1>
+    <h2>Here you can see my stunning house plant collection:</h2>
+    <div class="plants_collection">
+
+        <div class="prayer_plant"><img class="prayer_plant" src="images/prayer_plant.jpg" alt="here you can see my stunning Prayer Plant or Calathea medallion"><p>Prayer plant</p>
+        </div>
+        <div class="elephant_ear"><img class="elephant_ear" src="images/elephant-ear-plant.jpg" alt="beautiful elephant ear plant with a blue background"><p>Elephant ear plant</p></div>
+        <div class="spider_plant"><img class="spider_plant" src="images/curly-spider-plant.jpg" alt="curly spider plant in a grey pot"><p>Curly spider plant</p></div>
+        <div class="ivy"><img class="ivy" src="images/ivy.jpg" alt="english ivy in a grey pot with white background"><p>English ivy plant</p></div>
+        <div class="snake_plant"><img class="snake_plant" src="images/snake-plant.jpg" alt="snake plant also called mother in laws tongues in a dark grey pot"><p>Snake plant</p></div>
+        <div class="cheese_plant"><img class="cheese_plant" src="images/cheeseplant.jpg" alt="cheese plant in a traditional plastic pot"><p>Cheese plant</p></div>
+        <p>
+            <a href="form.php"><h1>Add new plants to the database here</h1></a>
+        </p>
+
+
+
+</div>
+</main>
+<footer>&copy; Copyright. All rights reserved. Karen Schulz, February 2021</footer>
+</body>
+
+</html>
+
