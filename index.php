1780fb4aac962412289a2748f6268dbf97a86770
Karen Schulz <sirk.schulz@gmail.com>
2021-02-09 08:44:18 +0000

    changed font. changed some paddings

--- index.php
+++ index.php
@@ -18,24 +18,108 @@
 
 <main>
     <h1>Hi!</h1>
-    <h2>Here you can see my stunning house plant collection:</h2>
+    <h1>Here you can see my stunning house plant collection:</h1>
+
     <div class="plants_collection">
+        <div class="prayer_plant">
+            <img class="prayer_plant" src="images/prayer_plant.jpg" alt="here you can see my stunning Prayer Plant or Calathea medallion">
+            <h2>Prayer plant</h2>
+            <h2>BOTANICAL NAME:</h2>
+            <ul>Calathea medallion</ul>
+            <h2>NICKNAME:</h2>
+            <ul>Prayer plant</ul>
+            <h2>PLANT TYPE:</h2>
+            <ul>Perennial, indoor</ul>
+            <h2>PLANT HEIGHT:</h2>
+            <ul>76.3 cm</ul>
+            <h2>POISENOUS:</h2>
+            <ul>No</ul>
 
-        <div class="prayer_plant"><img class="prayer_plant" src="images/prayer_plant.jpg" alt="here you can see my stunning Prayer Plant or Calathea medallion"><p>Prayer plant</p>
         </div>
-        <div class="elephant_ear"><img class="elephant_ear" src="images/elephant-ear-plant.jpg" alt="beautiful elephant ear plant with a blue background"><p>Elephant ear plant</p></div>
-        <div class="spider_plant"><img class="spider_plant" src="images/curly-spider-plant.jpg" alt="curly spider plant in a grey pot"><p>Curly spider plant</p></div>
-        <div class="ivy"><img class="ivy" src="images/ivy.jpg" alt="english ivy in a grey pot with white background"><p>English ivy plant</p></div>
-        <div class="snake_plant"><img class="snake_plant" src="images/snake-plant.jpg" alt="snake plant also called mother in laws tongues in a dark grey pot"><p>Snake plant</p></div>
-        <div class="cheese_plant"><img class="cheese_plant" src="images/cheeseplant.jpg" alt="cheese plant in a traditional plastic pot"><p>Cheese plant</p></div>
-        <p>
-            <a href="form.php"><h1>Add new plants to the database here</h1></a>
-        </p>
 
+        <div class="elephant_ear">
+            <img class="elephant_ear" src="images/elephant-ear-plant.jpg" alt="beautiful elephant ear plant with a blue background">
+            <h2>Elephant ear plant</h2>
+            <h2>BOTANICAL NAME:</h2>
+            <ul>Alocasia zebrina</ul>
+            <h2>NICKNAME:</h2>
+            <ul>Elephant ear plant</ul>
+            <h2>PLANT TYPE:</h2>
+            <ul>Perennial, indoor</ul>
+            <h2>PLANT HEIGHT:</h2>
+            <ul>55.5 cm</ul>
+            <h2>POISENOUS:</h2>
+            <ul>Yes</ul>
+        </div>
 
+        <div class="spider_plant">
+            <img class="spider_plant" src="images/curly-spider-plant.jpg" alt="curly spider plant in a grey pot">
+            <h2>Curly spider plant</h2>
+            <h2>BOTANICAL NAME:</h2>
+            <ul>Chlorophytum comosum</ul>
+            <h2>NICKNAME:</h2>
+            <ul>Curly spider plant; Variegated spider plant; Airplane plant</ul>
+            <h2>PLANT TYPE:</h2>
+            <ul>Perennial indoor</ul>
+            <h2>PLANT HEIGHT:</h2>
+            <ul>30 cm</ul>
+            <h2>POISENOUS:</h2>
+            <ul>No</ul>
+
+        </div>
+
+        <div class="ivy">
+            <img class="ivy" src="images/ivy.jpg" alt="english ivy in a grey pot with white background">
+            <h2>English ivy plant</h2>
+            <h2>BOTANICAL NAME:</h2>
+            <ul>Hedera helix</ul>
+            <h2>NICKNAME:</h2>
+            <ul>English ivy; Common ivy; European ivy</ul>
+            <h2>PLANT TYPE:</h2>
+            <ul>Evergreen climber</ul>
+            <h2>PLANT HEIGHT:</h2>
+            <ul>34.5</ul>
+            <h2>POISENOUS:</h2>
+            <ul>Yes</ul>
+        </div>
+
+        <div class="snake_plant">
+            <img class="snake_plant" src="images/snake-plant.jpg" alt="snake plant also called mother in laws tongues in a dark grey pot">
+            <h2>Snake plant</h2>
+            <h2>BOTANICAL NAME:</h2>
+            <ul>Sansevieria trifasciata</ul>
+            <h2>NICKNAME:</h2>
+            <ul>Snake plant; Mother-in-Laws tongue; Vipers bowst</ul>
+            <h2>PLANT TYPE:</h2>
+            <ul>Perennial, indoor</ul>
+            <h2>PLANT HEIGHT:</h2>
+            <ul>64.3 cm</ul>
+            <h2>POISENOUS:</h2>
+            <ul>Yes</ul>
+        </div>
+
+        <div class="cheese_plant">
+            <img class="cheese_plant" src="images/cheeseplant.jpg" alt="cheese plant in a traditional plastic pot">
+            <h2>Cheese plant</h2>
+            <h2>BOTANICAL NAME:</h2>
+            <ul>Monstera deliciosa</ul>
+            <h2>NICKNAME:</h2>
+            <ul>Cheese plant; Ceriman; Custard plant; Indian ivy; Fruit salad plant; Mexican breadfruit</ul>
+            <h2>PLANT TYPE:</h2>
+            <ul>Evergreen shrub, indoor</ul>
+            <h2>PLANT HEIGHT:</h2>
+            <ul>76.3cm</ul>
+            <h2>POISENOUS:</h2>
+            <ul>yes</ul>
+            </p>
 
 </div>
 </main>
+        <div>
+            <a href="form.php"><h1>Add new plants</h1></a>
+        </div>
+
+
 <footer>&copy; Copyright. All rights reserved. Karen Schulz, February 2021</footer>
 </body>
 
